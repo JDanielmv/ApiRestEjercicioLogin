d9591f3d1b30e8098a828d169daa72ba00e717ce
JDanielmv <yanda.mv98@gmail.com>
2022-02-15 09:24:07 +0100

    creacion de paquetes y clases

---
+++ api-rest-spring-ejerciciologin/src/main/java/com/formacionspring/app/apirest/ApiRestSpringEjercicioapirest1Application.java
@@ -0,0 +1,13 @@
+package com.formacionspring.app.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringEjercicioapirest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringEjercicioapirest1Application.class, args);
+	}
+
+}
